515f95a6ed8033f6c43da7ca2ee79d806dc999f1
Anil Pawar <anilpawar8111997@gmail.com>
2025-02-12 10:36:08 +0530

    host httpd server

--- nginx.ec2.vpc/ec2.tf
+++ nginx.ec2.vpc/ec2.tf
@@ -3,16 +3,12 @@
 resource "aws_instance" "nginxserver" {
   ami                         = "ami-00bb6a80f01f03502"
   instance_type               = "t3.nano"
+  key_name                    = "key"
   subnet_id                   = aws_subnet.public-subnet.id
   vpc_security_group_ids      = [aws_security_group.nginx-sg.id]
   associate_public_ip_address = true
 
-  user_data = <<-EOF
-            #!/bin/bash
-            sudo yum install nginx -y
-            sudo systemctl start nginx
-            sudo systemctl enable nginx
-            EOF
+  user_data = file ("userdata.sh")
 
   tags = {
     Name = "NginxServer"
