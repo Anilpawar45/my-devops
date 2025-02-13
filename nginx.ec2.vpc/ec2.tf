28e731ccd2b31f5359e601f1d6ad839a12028d5c
Anil Pawar <anilpawar8111997@gmail.com>
2025-02-13 19:26:40 +0530

    create my load balancer

--- nginx.ec2.vpc/ec2.tf
+++ nginx.ec2.vpc/ec2.tf
@@ -1,16 +1,16 @@
 
-# EC2 instance For Nginx setup
-resource "aws_instance" "nginxserver" {
-  ami                         = "ami-00bb6a80f01f03502"
-  instance_type               = "t3.nano"
-  key_name                    = "key"
-  subnet_id                   = aws_subnet.public-subnet.id
-  vpc_security_group_ids      = [aws_security_group.nginx-sg.id]
-  associate_public_ip_address = true
+# # EC2 instance For Nginx setup
+# resource "aws_instance" "nginxserver" {
+#   ami                         = "ami-00bb6a80f01f03502"
+#   instance_type               = "t3.nano"
+#   key_name                    = "key"
+#   subnet_id                   = aws_subnet.public-subnet.id
+#   vpc_security_group_ids      = [aws_security_group.nginx-sg.id]
+#   associate_public_ip_address = true
 
-  user_data = file ("userdata.sh")
+#   user_data = file("userdata.sh")
 
-  tags = {
-    Name = "NginxServer"
-  }
-}
+#   tags = {
+#     Name = "NginxServer"
+#   }
+# }
