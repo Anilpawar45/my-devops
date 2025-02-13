e5fd17320d5fc859de62078b6f2da3bb0b07b93e
Anil Pawar <anilpawar8111997@gmail.com>
2025-02-13 22:09:12 +0530

    launch templste new

--- scaling.tf
+++ scaling.tf
@@ -13,11 +13,6 @@ resource "aws_subnet" "public" {
   cidr_block        = "10.0.1.0/24"
   availability_zone = "us-east-1a"
 }
-resource "aws_subnet" "private" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.3.0/24"
-  availability_zone = "us-east-1b"
-}
 
 # Create an Internet Gateway
 resource "aws_internet_gateway" "gw" {
